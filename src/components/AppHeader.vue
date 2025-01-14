9216140c4070440f74353fbb230855ac97f55cd0
Harsh Narayan Jha <harshnj@proton.me>
2025-01-14 13:59:54 +0530

    complete landing section

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu'
+
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
+import ColorMode from '@/components/ColorMode.vue'
+import { GithubIcon } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="container w-full h-16 grid grid-cols-3 grid-rows-1 px-2 py-2 place-items-center">
+    <div class="place-self-start flex flex-row space-x-4 justify-center items-center">
+      <img alt="Logo" class="logo inline" src="@/assets/logo.svg" width="48" height="48" />
+      <span class="text-xl font-semibold tracking-tight">Ink'd Insults</span>
+    </div>
+
+    <NavigationMenu class="place-self-center">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <RouterLink to="/">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()"> Home </NavigationMenuLink>
+          </RouterLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <RouterLink to="/about">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()"> About </NavigationMenuLink>
+          </RouterLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
+    <div class="place-self-end inline-flex flex-row place-items-center space-x-4">
+      <ColorMode />
+      <div class="font-mono">|</div>
+      <a href="https://github.com/HarshNarayanJha/inkd-insults" target="_blank"><GithubIcon /></a>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { ArrowDownIcon } from 'lucide-vue-next'
+</script>
 
 <template>
   <main>
-    <h1>Hello World</h1>
+    <div class="min-h-svh flex flex-col justify-center items-center px-4 overflow-hidden">
+      <div class="z-10 text-center space-y-8">
+        <h1
+          class="text-8xl font-semibold tracking-tight drop-shadow-lg transform hover:scale-105 transition-transform duration-100 ease-in">
+          Love your handwriting?
+        </h1>
+        <h2 class="text-3xl font-medium tracking-wide">
+          Check yourself <ArrowDownIcon class="inline" />
+        </h2>
+        <h3
+          class="text-xl font-medium tracking-normal transform -rotate-2 hover:rotate-0 transition-transform">
+          You will hate it
+        </h3>
+      </div>
+      <div class="my-16 px-4 py-2 flex flex-row space-x-4 justify-between">
+        <Button variant="default">Upload Your Handwriting</Button>
+        <Button variant="outline">Try out an Example!</Button>
+      </div>
+    </div>
   </main>
 </template>
